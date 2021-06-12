8790b672f0b174544eb3433ae904eb5b6c93c869
Lakshmi Priya <mugundansv@gmail.com>
2021-06-12 20:57:00 +0530

    initial commit"

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=319225e7d19e9d3492f8c684fef2192d&query=${latitude},${longitude}`
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather services', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, {
+                weather: body.current.weather_descriptions[0],
+                temperature: body.current.temperature,
+                precipitation: body.current.precip
+            })
+        }
+    })
+}
+
+module.exports = forecast
